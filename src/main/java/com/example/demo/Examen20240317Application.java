2fdd2ad62f21d708676c6ab2d02309de8b67be03
Sharon Linares <akiralyn02@gmail.com>
2025-03-25 12:22:40 +0100

    primer commit

---
+++ src/main/java/com/example/demo/Examen20240317Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Examen20240317Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Examen20240317Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo/Examen20240317ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Examen20240317ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
