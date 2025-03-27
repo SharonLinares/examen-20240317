340dd1c0349db23f3571f32a976a7b2a4c55013f
Sharon Linares <akiralyn02@gmail.com>
2025-03-27 12:48:58 +0100

    cree factura2

--- src/main/java/servicio/Factura.java
+++ src/main/java/servicio/Factura.java
@@ -1,15 +1,48 @@
 package servicio;
 
+import dto.Persona;
+import dto.Producto;
+
 public class Factura {
 
-	
+	public void getFactura(Producto producto, Persona persona) {
+
+		double subTotal = producto.getCantidadProducto() * producto.getValorVenta();
+
+		System.out.println("nombre del producto: " + producto.getNombre());
+		System.out.println("nombre de la persona: " + persona.getNombre());
+
+	}
+
 	public void getFactura(String nombreProducto, int cantidad, float valorProducto) {
-		
-		
+
+		double subTotal = cantidad * valorProducto;
+		double descuento = 0.1;
+
 		System.out.println("nombre producto: " + nombreProducto);
 		System.out.println("cantidad: " + cantidad);
 		System.out.println("valor producto: " + valorProducto);
-		
+
+		if (subTotal > 100) {
+
+			double valorDescuento = subTotal * descuento;
+			double totalPago = subTotal - valorDescuento;
+			System.out.println("descuento de : " + valorDescuento);
+			System.out.println("total a pagar: " + totalPago);
+
+		} else {
+
+			System.out.println("total a pagar: " + subTotal);
+		}
+
 	}
-	
+
+	public static void main(String[] args) {
+
+		Factura factura = new Factura();
+
+		factura.getFactura("harina pan", 50, 3);
+
+	}
+
 }
---
+++ src/main/java/servicio/Factura2.java
@@ -0,0 +1,65 @@
+package servicio;
+
+import dto.Persona;
+import dto.Producto;
+
+/*
+ * 1. si el nombre es igual checho hágale un descuento del 10% 2. si el nombre
+ * es igual sharon hágale un descuento del 5%. 3. si el nombre es igual diego
+ * hágale un descuento del 50% 4. cualquier otro nombre sin descuento.
+ * 
+ */
+public class Factura2 {
+
+	public void getFactura2(Persona persona, Producto producto) {
+
+		double descuentoChecho = 0.1;
+		double descuentoSharon = 0.05;
+		double descuentoDiego = 0.5;
+		double subTotal = producto.getCantidadProducto() * producto.getValorVenta();
+		double valorDescuento = 0.0;
+		double totalPago = 0.0;
+
+		System.out.println("nombre producto: " + producto.getNombre());
+		System.out.println("cantidad: " + producto.getCantidadProducto());
+		System.out.println("valor producto: " + producto.getValorVenta());
+
+		switch (persona.getNombre()) {
+		case "Checho": {
+			valorDescuento = subTotal * descuentoChecho;
+			totalPago = subTotal - valorDescuento;
+			System.out.println(persona.getNombre() + " tu pago es de: " + totalPago);
+
+			break;
+		}
+
+		case "Sharon": {
+			valorDescuento = subTotal * descuentoSharon;
+			totalPago = subTotal - valorDescuento;
+			System.out.println(persona.getNombre() + " tu pago es de: " + totalPago);
+			break;
+		}
+		case "Diego": {
+			valorDescuento = subTotal * descuentoDiego;
+			totalPago = subTotal - valorDescuento;
+			System.out.println(persona.getNombre() + " tu pago es de: " + totalPago);
+			break;
+		}
+		default:
+			valorDescuento = 0.0;
+			totalPago = subTotal;
+		}
+
+	}
+
+	public static void main(String[] args) {
+
+		Factura2 factura2 = new Factura2();
+
+		factura2.getFactura2("checho", 22);
+		
+	}
+}
+
+
+
